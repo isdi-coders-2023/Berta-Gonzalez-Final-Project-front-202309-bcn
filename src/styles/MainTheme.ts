d21d55a02e7418a6786da415bd78bd37f5ce3a26
Berta González <bertagonzalezmartin@gmail.com>
2023-11-29 14:10:25 +0100

    Add mainTheme styles

---
+++ src/styles/MainTheme.ts
@@ -0,0 +1,19 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  color: {
+    mainFontColor: "#45484A",
+    lightFontColor: "#fff",
+    mainColor: "#b1cbbb",
+    secondaryColor: "#c94c4c",
+    buttonMainColor: "rgba(201, 76, 76, 0.74)",
+    buttonDesableColor: " rgba(201, 76, 76, 0.31)",
+    okFeedbackBackgroundColor: "#5db381",
+    failFeedbackBackgroundColor: "#b71c1c",
+  },
+  typography: {
+    mainFontFamily: "'Patrick Hand', sans-serif",
+  },
+};
+
+export default mainTheme;
