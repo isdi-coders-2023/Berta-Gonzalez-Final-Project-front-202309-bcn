93d9e786e4dae7e73fafa3e4de4d377496c924bf
Berta González <bertagonzalezmartin@gmail.com>
2023-11-30 12:43:19 +0100

    Add ballsReducer test

---
+++ src/store/features/balls/ballsReducer.test.tsx
@@ -0,0 +1,21 @@
+import ballsMock from "../../../mocks/ballsMock";
+import { BallsStateStructure } from "../types";
+import { ballsReducer, loadBallsActionCreator } from "./ballsSlice";
+
+describe("Given a ballsReducer", () => {
+  describe("When it receives an empty list of balls", () => {
+    test("Then it should return a list with 'Harry Potter crew' and 'My neighbor Totoro' balls", () => {
+      const ballsList = ballsMock;
+      const currentBallsState: BallsStateStructure = {
+        balls: [],
+      };
+
+      const newBallsState = ballsReducer(
+        currentBallsState,
+        loadBallsActionCreator(ballsList),
+      );
+
+      expect(newBallsState.balls).toStrictEqual(ballsList);
+    });
+  });
+});
