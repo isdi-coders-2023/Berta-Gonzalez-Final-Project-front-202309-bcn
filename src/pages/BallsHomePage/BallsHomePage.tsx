f25dfa5bb79eb0fd7315cbe96139ae03cda3a1ea
Berta González <bertagonzalezmartin@gmail.com>
2023-11-30 13:08:01 +0100

    Add useEfect dispatch function in homepage

--- src/pages/BallsHomePage/BallsHomePage.tsx
+++ src/pages/BallsHomePage/BallsHomePage.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
+
 import BallsHomePageStyled from "./BallsHomePageStyled";
+import { loadBallsActionCreator } from "../../store/features/balls/ballsSlice";
+import ballsMock from "../../mocks/ballsMock";
+import { useAppDispatch } from "../../store/hooks";
 
 const BallsHomePage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadBallsActionCreator(ballsMock));
+  }, [dispatch]);
+
   return (
     <BallsHomePageStyled>
       <h1 className="homepage-text">Mark your have and lack</h1>
