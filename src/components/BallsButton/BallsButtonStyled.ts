bbbbc1ee0baef6a8999fb30db9c589af936021e8
Berta González <bertagonzalezmartin@gmail.com>
2023-12-01 20:14:02 +0100

    Add button main styles

---
+++ src/components/BallsButton/BallsButtonStyled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const BallsButtonStyled = styled.button`
+  width: 139px;
+  height: 48px;
+  background-color: ${({ theme }) => theme.color.buttonMainColor};
+  color: ${({ theme }) => theme.color.mainFontColor};
+  font-size: 1.438rem;
+  border-radius: 5px;
+`;
+
+export default BallsButtonStyled;
