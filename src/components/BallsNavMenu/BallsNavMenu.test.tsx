b873810c11ed5c2f40411616809f28f26a87dd1d
Berta González <bertagonzalezmartin@gmail.com>
2023-12-01 18:30:24 +0100

    Fix navmenu test

--- src/components/BallsNavMenu/BallsNavMenu.test.tsx
+++ src/components/BallsNavMenu/BallsNavMenu.test.tsx
@@ -5,10 +5,10 @@ import BallsNavMenu from "./BallsNavMenu";
 describe("Given BallsNavMenu component", () => {
   describe("When it is rendered", () => {
     test("Then it should show an 'Add' text", () => {
-      const expectedText = "Add";
+      const expectedText = "add-form";
 
       customRender(<BallsNavMenu />);
-      const addText = screen.getByText(expectedText);
+      const addText = screen.getByRole("link", { name: expectedText });
 
       expect(addText).toBeInTheDocument();
     });
