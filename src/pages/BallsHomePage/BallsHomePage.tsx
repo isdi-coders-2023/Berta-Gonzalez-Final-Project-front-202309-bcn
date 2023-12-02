6a7819bdf65c4f5f4761ffd2a3f40c599597c07f
Berta González <bertagonzalezmartin@gmail.com>
2023-12-02 21:08:27 +0100

    Add useBallsApi and useEffect functions in homepage file

--- src/pages/BallsHomePage/BallsHomePage.tsx
+++ src/pages/BallsHomePage/BallsHomePage.tsx
@@ -1,12 +1,22 @@
+import { useAppDispatch } from "../../store/hooks";
+import { useEffect } from "react";
+import BallsList from "../../components/BallsList/BallsList";
+import useBallsApi from "../../hooks/useBallsApi";
 import BallsHomePageStyled from "./BallsHomePageStyled";
 import { loadBallsActionCreator } from "../../store/features/balls/ballsSlice";
-import ballsMock from "../../mocks/ballsMock";
-import { useAppDispatch } from "../../store/hooks";
-import BallsList from "../../components/BallsList/BallsList";
 
 const BallsHomePage = (): React.ReactElement => {
   const dispatch = useAppDispatch();
-  dispatch(loadBallsActionCreator(ballsMock));
+  const { getBallsApi } = useBallsApi();
+
+  useEffect(() => {
+    (async () => {
+      const { balls } = await getBallsApi();
+
+      dispatch(loadBallsActionCreator(balls));
+    })();
+  }, [dispatch, getBallsApi]);
+
   return (
     <BallsHomePageStyled>
       <h1 className="homepage-text">Mark your have and lack</h1>
