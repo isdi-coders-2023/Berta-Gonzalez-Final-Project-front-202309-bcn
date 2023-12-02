628644ad29ae9f9ecb69e5c1301863c33f0532aa
Berta González <bertagonzalezmartin@gmail.com>
2023-12-02 20:54:18 +0100

    Add handler route in handlers file

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,7 +3,7 @@ import ballsMock from "./ballsMock";
 
 const urlApi = import.meta.env.VITE_API_URL;
 
-export const handler = [
+const handler = [
   http.get(`${urlApi}/balls`, () => {
     return HttpResponse.json(ballsMock);
   }),
