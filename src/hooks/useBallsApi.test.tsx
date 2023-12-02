9058b1b2779bfc4791ebc8b7d60f8e848a06a1d4
Berta González <bertagonzalezmartin@gmail.com>
2023-12-02 21:07:27 +0100

    Add useBallsApi test

---
+++ src/hooks/useBallsApi.test.tsx
@@ -0,0 +1,21 @@
+import { renderHook } from "@testing-library/react";
+import ballsMock from "../mocks/ballsMock";
+import useBallsApi from "./useBallsApi";
+
+describe("Given a useBallsApi custom hook", () => {
+  describe("When it receives information of 'Harry Potter crew', 'My neighbour Totoro' and 'Mario Bros'", () => {
+    test("Then it should show 'Harry Potter crew', 'My neighbour Totoro' and 'Mario Bros' full information", async () => {
+      const expectedBalls = ballsMock;
+
+      const {
+        result: {
+          current: { getBallsApi },
+        },
+      } = renderHook(() => useBallsApi());
+
+      const currentBalls = await getBallsApi();
+
+      expect(currentBalls).toStrictEqual(expectedBalls);
+    });
+  });
+});
