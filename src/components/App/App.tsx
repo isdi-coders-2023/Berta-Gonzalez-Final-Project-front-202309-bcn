9359baa05701a1751db344fc2cfede015505b86e
Berta González <bertagonzalezmartin@gmail.com>
2023-12-04 14:52:43 +0100

    Add loading in app component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,16 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import BallsHeader from "../BallsHeader/BallsHeader";
 import BallsHomePage from "../../pages/BallsHomePage/BallsHomePage";
 import MainComponentStyled from "../MainComponentStyled/MainComponentStyled";
+import BallsLoading from "../BallsLoading/BallsLoading";
+import { useAppSelector } from "../../store/hooks";
 
 const App = (): React.ReactElement => {
+  const uiState = useAppSelector((state) => state.uiState);
+
   return (
     <>
       <BallsHeader />
+      {uiState.isLoading && <BallsLoading />}
       <MainComponentStyled>
         <Routes>
           <Route path="/" element={<Navigate to="/balls" />} />
