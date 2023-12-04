52bef3078e28dd968ff15dfe7566380fb607ae0b
Berta González <bertagonzalezmartin@gmail.com>
2023-12-04 14:53:12 +0100

    Add links test in App test

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,17 +1,34 @@
 import { screen } from "@testing-library/react";
-import customRender from "../../testUtils/customRender";
+import { customRender } from "../../testUtils/customRender";
 import App from "./App";
+import ballsMock from "../../mocks/ballsMock";
 
 describe("Given an App component", () => {
+  const mockData = ballsMock;
+
   describe("When it's rendered", () => {
     test("Then it should show 'Nerdmas Balls' logo", () => {
       const expectedAltText = "Nerdmas Balls logo";
 
-      customRender(<App />);
+      customRender(<App />, mockData);
 
-      const headerLogo = screen.getByAltText(expectedAltText);
+      const altText = screen.getByRole("img", { name: expectedAltText });
 
-      expect(headerLogo).toBeInTheDocument();
+      expect(altText).toBeInTheDocument();
+    });
+
+    describe("When it receives a click on the 'Home' link", () => {
+      test("Then it should show a title 'Mark your have and lack' on a Heading", () => {
+        const homepageTitle = "Mark your have and lack";
+        const homeLink = "Home";
+
+        customRender(<App />, mockData);
+        const link = screen.getByRole("link", { name: homeLink });
+        const title = screen.getByRole("heading", { name: homepageTitle });
+
+        expect(title).toBeInTheDocument();
+        expect(link).toBeInTheDocument();
+      });
     });
   });
 });
