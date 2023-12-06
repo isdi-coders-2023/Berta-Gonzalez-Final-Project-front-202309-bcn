f480978f8b6a22f0632df46be422cec98f555c1f
Berta González <bertagonzalezmartin@gmail.com>
2023-12-06 20:30:51 +0100

    Add load tostify message error test

--- src/pages/BallsHomePage/BallsHomePage.test.tsx
+++ src/pages/BallsHomePage/BallsHomePage.test.tsx
@@ -2,12 +2,16 @@ import { screen } from "@testing-library/dom";
 import { customRender } from "../../testUtils/customRender";
 import BallsHomePage from "./BallsHomePage";
 import ballsMock from "../../mocks/ballsMock";
+import server from "../../mocks/node";
+import { errorHandlers } from "../../mocks/handlers";
+import { waitFor } from "@testing-library/react";
 
 describe("Given a BallsHomePage component", () => {
+  const mockData = ballsMock;
+
   describe("When it is rendered", () => {
     test("Then it should show the title 'Mark your have and lack'", () => {
       const expectedTitle = "Mark your have and lack";
-      const mockData = ballsMock;
 
       customRender(<BallsHomePage />, mockData);
       const title = screen.getByRole("heading", {
@@ -15,6 +19,19 @@ describe("Given a BallsHomePage component", () => {
       });
 
       expect(title).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered but the list of balls doesn't appear", () => {
+    test("Then it show the error message 'Nerdmas Balls could not be loaded'", async () => {
+      const expectedErrorMessage = "Nerdmas Balls could not be loaded";
+
+      server.use(...errorHandlers);
+      customRender(<BallsHomePage />, mockData);
+
+      await waitFor(() => {
+        expect(screen.getByText(expectedErrorMessage)).toBeInTheDocument();
+      });
     });
   });
 });
